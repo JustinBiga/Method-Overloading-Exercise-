15b996173e7f79f287d521056af2eca03a41e0db
JustinBiga <BISENGOJ@GMAIL.COM>
2024-04-01 14:59:38 -0400

    Inital commit

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -4,7 +4,49 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var a = 40;
+            var b = 35;
+            var answer = Add(a, b);
+
+            var x = 35.7m;
+            var y = 56.3m;
+
+            var decimalAnswer = Add(x, y);
+
+            var Answer3 = Add(3, 18, true);
+
+            Console.WriteLine($"integer Add: {answer} decimal Add: {decimalAnswer} \n" +
+                $"{Answer3}");
         }
+        public static int Add(int num1, int num2)
+        {
+
+            return num1 + num2;
+        }
+
+        public static decimal Add(decimal num1, decimal num2)
+        {
+ 
+            return num1 + num2;
+        }
+
+        public static string Add(int num1, int num2, bool dollars)
+          
+        {
+            var sum = num1 + num2;
+            if (dollars == true && sum == 1)
+            {
+                return $"{sum} dollar";
+            }
+            else if (dollars == false && sum == 1)
+            {
+                return $"{sum} dollar";
+            }
+            else
+            {
+                return $"{sum} dollars";
+            }
+        }
+
     }
 }
